7d4863856ac9f8a3a24c38d4395f5e5cf5f32954
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-12 21:27:20 +0900

    [Bronze V] Title: A+B - 3, Time: 120 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let testCase = Number(input[0]);
+
+let answer = '';
+for (let i = 1; i <= testCase; i++) {
+    let data = input[i].split(' ');
+    let [a, b] = data.map(Number);
+    answer += `${a+b}\n`;
+}
+
+
+console.log(answer);
