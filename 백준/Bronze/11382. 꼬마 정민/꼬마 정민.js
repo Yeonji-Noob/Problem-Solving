6741b39aba4d884104113fd3b4be992e1d17e513
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-15 20:28:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -1,6 +1,3 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-let [a, b, c] = input[0].split(' ').map(Number);
+let [a, b, c] = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
 
 console.log(a+b+c);
