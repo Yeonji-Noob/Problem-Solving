17c6f1f6b676a0d4053c8deec49fb20d6f644d2b
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-15 19:22:13 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.js
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -3,4 +3,7 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let [a, b, c] = input[0].split(' ').map(Number);
 
-console.log(`${(a+b)%c}\n${((a%c)+(b%c))%c}\n${(a*b)%c}\n${((a%c)*(b%c))%c}`); 
+console.log(`${(a+b)%c}
+${((a%c)+(b%c))%c}
+${(a*b)%c}
+${((a%c)*(b%c))%c}`); 
