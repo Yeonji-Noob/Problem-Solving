c9aca735ce28984ef74ab461abc4b5c5df305adf
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-19 08:35:12 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,9 +1,6 @@
 let fs = require('fs');
-let input = fs.readFileSync('dev/stdin').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let line = input[0].split(' ');
+let [a, b] = input[0].split(' ').map(Number);
 
-let a = parseInt(line[0]);
-let b = parseInt(line[1]);
-
-console.log(a+b);
+console.log(a + b);
