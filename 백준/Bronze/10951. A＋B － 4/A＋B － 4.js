0ab636343f7147481898e17412ceab7435abaf74
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-19 09:42:45 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -1,6 +1,6 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-for(let i = 0; i<input.length; i++){
-    let answer = input[i].split(' ');
-    console.log(Number(answer[0])+Number(answer[1]));
-}
+input.forEach(line => {
+    let [a, b] = line.split(' ');
+    console.log(+a + +b);
+});
