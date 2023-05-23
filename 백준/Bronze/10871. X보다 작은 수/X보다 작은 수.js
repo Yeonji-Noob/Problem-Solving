358404e7fac1581b90bbba849946d65fbcf7d792
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-23 16:08:26 +0900

    [Bronze V] Title: X보다 작은 수, Time: 128 ms, Memory: 11068 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let [N, X] = input[0].split(' ').map(Number);
+let arr = input[1].split(' ').map(Number);
+
+let answer = [];
+for(let i = 0; i < N; i++){
+    if( X > arr[i]) answer.push(arr[i]);
+}
+    console.log(answer.join(' '));
