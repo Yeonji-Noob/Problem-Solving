8ee782253cb6af68bccf8ed369d5ba7c4b8c3a1f
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-05-25 18:09:15 +0900

    [Bronze I] Title: 평균, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -1,11 +1,14 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const number = Number(input.shift());
-const score = input[0].split(' ').map(Number);
-const M = Math.max(...score);
-const newScore = score.map(item => item/M * 100);
-const sum = newScore.reduce((acc, cur) => acc + cur);
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let number = Number(input.shift());
+let score = input[0].split(' ').map(Number);
+let M = Math.max(...score);
+
+let sum = 0;
+for(let i = 0; i<number; i++){
+    sum += score[i]/M * 100;
+}
     
-console.log(sum/number);
+console.log((sum/number).toFixed(5));
 
 
 
