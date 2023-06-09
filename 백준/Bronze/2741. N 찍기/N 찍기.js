3c4dad5c974c7a08834795153581af7c08a32735
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-06-09 21:36:41 +0900

    [Bronze V] Title: N 찍기, Time: 236 ms, Memory: 22708 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]);
+
+let answer = '';
+for(let i = 1; i<=N; i++){
+    answer += `${i}\n`;
+}
+
+console.log(answer);
