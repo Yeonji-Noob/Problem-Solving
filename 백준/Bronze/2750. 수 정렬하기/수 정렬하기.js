584c177270011cde7f514e2fd79174505a95ede8
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-07-08 11:45:49 +0900

    [Bronze II] Title: 수 정렬하기, Time: 128 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,8 +1,6 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let N = input.shift();
-let num = input.sort((a, b) => a - b);
+let sorted = input.sort((a,b) => a-b);
 
-for(let i = 0; i < N; i++){
-    console.log(input[i]);
-}
+console.log(input.join('\n'));
