f137d5d0762b20223804132cbbd4f48accec5347
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-07-08 20:48:28 +0900

    [Silver V] Title: K번째 수, Time: 4564 ms, Memory: 566464 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -1,12 +1,7 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 let [N, K] = input[0].split(' ').map(Number);
-let num = input[1].split(' ').map(Number);
-
-let A = [];
-for(let i = 0; i<N; i++){
-    A.push(num[i]);
-}
+let A = input[1].split(' ').map(Number);
 
 A.sort((a, b) => a - b);
 
