d5f4dae07524d179c99dd0f40feb1da7563d9c38
Yeonji-Noob <121682565+Yeonji-Noob@users.noreply.github.com>
2023-07-09 21:29:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 524 ms, Memory: 60236 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,24 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]);
+
+let arr = [];
+for(let i = 1; i <= N; i++){
+    let [x, y] = input[i].split(' ').map(Number);
+    arr.push([x, y]);
+}
+
+let compare = (a, b) => {
+    if(a[0] !== b[0]) return a[0] - b[0];
+    else return a[1] - b[1];
+}
+
+arr.sort(compare);
+
+let answer = '';
+for (let point of arr) {
+    answer += point[0] + ' ' + point[1] + '\n';
+}
+
+console.log(answer);
+
